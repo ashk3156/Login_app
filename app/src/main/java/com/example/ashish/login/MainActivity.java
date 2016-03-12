e03a5e661c45154feb450b639767844ea306e5d5
Ashish <ashk3156@gmail.com>
2016-03-12 13:03:14 +0530

    commit main

---
+++ app/src/main/java/com/example/ashish/login/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.ashish.login;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.ashish.login.activity.LoginActivity;
+import com.example.ashish.login.helper.SQLiteHandler;
+import com.example.ashish.login.helper.SessionManager;
+
+import java.util.HashMap;
+
+public class MainActivity extends Activity {
+
+    private TextView txtName;
+    private TextView txtEmail;
+    private Button btnLogout;
+
+    private SQLiteHandler db;
+    private SessionManager session;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtName = (TextView) findViewById(R.id.name);
+        txtEmail = (TextView) findViewById(R.id.email);
+        btnLogout = (Button) findViewById(R.id.btnLogout);
+        // SqLite database handler
+        db = new SQLiteHandler(getApplicationContext());
+
+        // session manager
+        session = new SessionManager(getApplicationContext());
+        if (!session.isLoggedIn()) {
+            logoutUser();
+        }
+        // Fetching user details from sqlite
+        HashMap<String, String> user = db.getUserDetails();
+
+        String name = user.get("name");
+        String email = user.get("email");
+
+        // Displaying the user details on the screen
+        txtName.setText(name);
+        txtEmail.setText(email);
+
+        // Logout button click event
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                logoutUser();
+            }
+        });
+    }
+
+    private void logoutUser() {
+        session.setLogin(false);
+
+        db.deleteUsers();
+
+        // Launching the login activity
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
---
+++ app/src/main/java/com/example/ashish/login/app/AppConfig.java
@@ -0,0 +1,12 @@
+package com.example.ashish.login.app;
+
+/**
+ * Created by Ashish on 2/28/2016.
+ */
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://172.16.41.146/android_login_api/include/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://172.16.41.146/android_login_api/include/register.php";
+}
